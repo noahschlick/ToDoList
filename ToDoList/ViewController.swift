503e3ab2932ba8e474dca261862dc73c86f9ee58
noahschlick <49171976+noahschlick@users.noreply.github.com>
2020-06-01 13:42:08 -0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Noah Schlickeisen on 6/1/20.
+//  Copyright © 2020 Noah Schlickeisen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
